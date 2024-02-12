63947d114441a97ebaaedc1ba7d19b608f14fb3a
dhruvi6071 <dhruvi6071@gmail.com>
2024-02-12 19:24:01 +0530

    task-input

--- task-manager/task-manager/src/components/Input.jsx
+++ task-manager/task-manager/src/components/Input.jsx
@@ -1,9 +1,9 @@
 export default function Input({label, textarea, ...props}) {
     return(
         <>
-        <p>
-            <label>{label}</label>
-            {textarea ? <textarea {...props}></textarea>: <input{...props}></input>}
+        <p className="flex flex-col gap-1 my-4">
+            <label className="text-sm font-bold uppercase">{label}</label>
+            {textarea ? <textarea className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props}></textarea>: <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"{...props}></input>}
         </p>
         </>
     );
